57ba5fcfd334374d1af5d253b358b577fc0bba69
Dan Shields <nukemandan@protonmail.com>
2020-10-19 12:52:47 -0600

    add BlockInfo and gh-pages to deploy

--- src/App.js
+++ src/App.js
@@ -1,49 +1,64 @@
-import React, { useState, createRef } from 'react';
-import { Container, Dimmer, Loader, Grid, Sticky, Message } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
+import React, { useState, createRef } from "react";
+import {
+  Container,
+  Dimmer,
+  Loader,
+  Grid,
+  Sticky,
+  Message,
+} from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
-import { SubstrateContextProvider, useSubstrate } from './substrate-lib';
-import { DeveloperConsole } from './substrate-lib/components';
+import { SubstrateContextProvider, useSubstrate } from "./substrate-lib";
+import { DeveloperConsole } from "./substrate-lib/components";
 
-import AccountSelector from './AccountSelector';
-import Balances from './Balances';
-import BlockNumber from './BlockNumber';
-import Events from './Events';
-import Interactor from './Interactor';
-import Metadata from './Metadata';
-import NodeInfo from './NodeInfo';
-import TemplateModule from './TemplateModule';
-import Transfer from './Transfer';
-import Upgrade from './Upgrade';
+import AccountSelector from "./AccountSelector";
+import Balances from "./Balances";
+import BlockNumber from "./BlockNumber";
+import BlockInfo from "./BlockInfo";
+import Events from "./Events";
+import Interactor from "./Interactor";
+import Metadata from "./Metadata";
+import NodeInfo from "./NodeInfo";
+import TemplateModule from "./TemplateModule";
+import Transfer from "./Transfer";
+import Upgrade from "./Upgrade";
 
-function Main () {
+function Main() {
   const [accountAddress, setAccountAddress] = useState(null);
   const { apiState, keyring, keyringState, apiError } = useSubstrate();
   const accountPair =
     accountAddress &&
-    keyringState === 'READY' &&
+    keyringState === "READY" &&
     keyring.getPair(accountAddress);
 
-  const loader = text =>
+  const loader = (text) => (
     <Dimmer active>
-      <Loader size='small'>{text}</Loader>
-    </Dimmer>;
+      <Loader size="small">{text}</Loader>
+    </Dimmer>
+  );
 
-  const message = err =>
+  const message = (err) => (
     <Grid centered columns={2} padded>
       <Grid.Column>
-        <Message negative compact floating
-          header='Error Connecting to Substrate'
-          content={`${JSON.stringify(err, null, 4)}`}
+        <Message
+          negative
+          compact
+          floating
+          header="Error Connecting to Substrate"
+          content={`${err}`}
         />
       </Grid.Column>
-    </Grid>;
+    </Grid>
+  );
 
-  if (apiState === 'ERROR') return message(apiError);
-  else if (apiState !== 'READY') return loader('Connecting to Substrate');
+  if (apiState === "ERROR") return message(apiError);
+  else if (apiState !== "READY") return loader("Connecting to Substrate");
 
-  if (keyringState !== 'READY') {
-    return loader('Loading accounts (please review any extension\'s authorization)');
+  if (keyringState !== "READY") {
+    return loader(
+      "Loading accounts (please review any extension's authorization)"
+    );
   }
 
   const contextRef = createRef();
@@ -54,12 +69,15 @@ function Main () {
         <AccountSelector setAccountAddress={setAccountAddress} />
       </Sticky>
       <Container>
-        <Grid stackable columns='equal'>
+        <Grid stackable columns="equal">
           <Grid.Row stretched>
             <NodeInfo />
             <Metadata />
             <BlockNumber />
             <BlockNumber finalized />
+          </Grid.Row>
+          <Grid.Row centered>
+            <BlockInfo />
           </Grid.Row>
           <Grid.Row stretched>
             <Balances />
@@ -82,7 +100,7 @@ function Main () {
   );
 }
 
-export default function App () {
+export default function App() {
   return (
     <SubstrateContextProvider>
       <Main />
